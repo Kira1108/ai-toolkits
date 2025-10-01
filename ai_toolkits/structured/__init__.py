5df6c0a377f50a7e9610eb16521a5114b4a2d3ab
Kira1108 <327485253@163.com>
2025-10-01 19:53:45 +0800

    Initial commit

--- ai_toolkits/structured/__init__.py
+++ ai_toolkits/structured/__init__.py
@@ -1,1 +1,2 @@
-from . extractor import create_object_openai
+from .extractor import (acreate_object_openai, acreate_object_openai_safe,
+                        acreate_objects_openai_safe, create_object_openai)
--- ai_toolkits/structured/extractor.py
+++ ai_toolkits/structured/extractor.py
@@ -1,6 +1,12 @@
+import asyncio
+
 import instructor
+from openai import Client, AsyncClient
 from pydantic import BaseModel
-from openai import Client
+from ai_toolkits.llms import create_async_client
+import logging
+
+logger = logging.getLogger(__name__)
 
 def create_object_openai(
     output_cls:BaseModel, 
@@ -31,6 +37,8 @@ async def acreate_object_openai(
         raise ValueError("Please provide an OpenAI client.")
     
     client = instructor.from_openai(client)
+
+    logger.info(f"Creating object of type {output_cls.__name__} with prompt: {prompt[:30]}...")  # Log the prompt (truncated for brevity)
     return await client.chat.completions.create(
         model="gpt-4o",
         response_model=output_cls,
@@ -48,3 +56,40 @@ async def acreate_object_openai_safe(
         return await acreate_object_openai(output_cls, prompt, client)
     except Exception as e:
         return ErrorResponse(error=str(e))
+
+
+async def acreate_objects_openai_safe(
+    prompt:str, 
+    output_classes:list[BaseModel],
+    client:AsyncClient = None) -> dict:
+    
+    if client is None:
+        client = create_async_client()
+    
+    """
+    Give a conversation between a police officer and a civilian, classify the case into multiple categories.
+    The output is defined by a series of pydantic models.
+    
+    Args:
+        conversation (str): The conversation text to be analyzed.
+
+    Returns: A dictionary where keys are the names of the classification categories
+        and values are the corresponding classification results.
+    """
+    class_names = [cls.__name__ for cls in output_classes]
+    
+    tasks = [
+        acreate_object_openai_safe(
+            output_cls=cls, 
+            prompt=prompt,
+            client=client
+        )
+        for cls in output_classes
+    ]
+    
+    objects = await asyncio.gather(*tasks)
+    
+    return {
+        class_name: obj.model_dump() 
+        for class_name, obj in zip(class_names, objects)
+    }
