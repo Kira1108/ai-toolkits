9fd6bb3ea713ff05e34d12868ac693679d0cb542
Kira1108 <327485253@163.com>
2025-10-01 19:29:46 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# The fucking AI toolkits
+# AI toolkits
 
-Environment variables required:
-- OPENAI_API_KEY
-- AZURE_OPENAI_API_KEY
-- AZURE_OPENAI_ENDPOINT
+1. Put environment variables in home directory, `~/.env`
+
+```
+OPENAI_API_KEY="your_openai_api_key"
+AZURE_OPENAI_API_KEY="your azure_openai_api_key"
+AZURE_OPENAI_ENDPOINT="your azure_openai_endpoint"
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 from setuptools import find_packages
 
+def read_requirements():
+    with open('requirements.txt', 'r') as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith('#')]
+
 setup(name='ai_toolkits',
       version='0.0.1',
-      description='the fucking ai toolkits',
+      description='AI toolkits for LLM integrations and structured data extraction',
       author='The fastest man alive.',
       packages=find_packages(),
-      install_requires=[])
+      install_requires=read_requirements())
