c007b98e7ce42a90b5e38f64458b91e7340d8679
王欢 <wanghuanr@yxqiche.com>
2025-10-11 13:53:29 +0800

    add asr

--- ai_toolkits/audio/microphone.py
+++ ai_toolkits/audio/microphone.py
@@ -7,7 +7,8 @@ class MicrophoneClient:
         audio_input_queue: asyncio.Queue = None,
         sampling_rate: int = 8000, 
         chunk_ms: int = 40, 
-        channels: int = 1):
+        channels: int = 1,
+        duration:int = 30):
         
         self.audio_input_queue = audio_input_queue if audio_input_queue is not None else asyncio.Queue()
         self.sampling_rate = sampling_rate
@@ -15,6 +16,7 @@ class MicrophoneClient:
         self.channels = channels
             
         self.port_audio = pyaudio.PyAudio()
+        self.duration = duration
         self.stream = self.port_audio.open(
             format=pyaudio.paInt16,
             channels=channels,
@@ -25,8 +27,11 @@ class MicrophoneClient:
         
     async def record(self):
         print("Starting to send audio")
+        start = asyncio.get_event_loop().time()
         while self.stream.is_active():
-
+            if asyncio.get_event_loop().time() - start > self.duration:
+                await self.audio_input_queue.put(json.dumps({"type": "end"}))
+                break
             data = self.stream.read(self.chunk_size, exception_on_overflow=False)
             await self.audio_input_queue.put(data)
             await asyncio.sleep(0.01)
