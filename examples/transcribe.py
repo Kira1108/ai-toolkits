aa8e74c1801cb027511a2217ba6a8bac8bbcdd87
Kira1108 <327485253@163.com>
2025-10-12 15:48:21 +0800

    Initial commit

--- examples/transcribe.py
+++ examples/transcribe.py
@@ -1,5 +1,6 @@
 from ai_toolkits.audio.transcribe import real_time_transcribe   
 import asyncio
-import nest_asyncio
-nest_asyncio.apply()    
-asyncio.run(real_time_transcribe())
+
+
+if __name__ == "__main__": 
+    asyncio.run(real_time_transcribe())
